1d4fb7286494f2030b45954fc3db4f575e2caec0
Deeppjp116 <deeppjp1656@gmail.com>
2024-12-03 09:27:20 +0530

    Added new lines

--- day2/pro1.py
+++ day2/pro1.py
@@ -3,26 +3,16 @@ from matplotlib.pyplot import *
 from math import *
 
 
-t = range(100000)
-# tsq = sqrt(t)
+a = array([1, 2, 3, 4])
+b = array([2, 3, 4, 5])
+print(a + b)
 
 
-t = linspace(0, 10, 1000)
-print(t)
+x = linspace(0.0, 10.0, 200)
+x *= 2 * pi / 10
 
+y = sin(x)
+y = cos(x)
 
-# print(tsq)
-# t = array(t)
-# tsq = t * t
-
-
-# print(tsq)
-# plot(t, tsq)
-#
-
-
-def dosum(a, b):
-    return a + b
-
-
-print(dosum([1, 2], [2, 3]))
+x[0] = -1
+print(x[0], x[-1])
