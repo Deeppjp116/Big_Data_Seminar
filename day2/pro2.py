d2061c6802a687296ccb8834ce2359c30e2a4321
Deeppjp116 <deeppjp1656@gmail.com>
2024-12-03 10:02:19 +0530

    Added new lines

--- day2/pro2.py
+++ day2/pro2.py
@@ -2,6 +2,21 @@ from numpy import *
 from matplotlib.pyplot import *
 from math import *
 
-a = array([[1, 2, 3, 4], [11, 12, 13, 14]])
+# a = array([[1, 2, 3, 4], [11, 12, 13, 14]])
 
-print(a.shape)
+# print(a.shape)
+
+# a = array([[1, 2, 3, 4], [4, 5, 6, 0], [11, 12, 13, 14]])
+# print(a[0, 1:3])
+
+# a= loadtxt(./circulate/lena.png)
+
+# a = array([1, 2, 3], dtype=float)
+
+# ones_like(a)
+# array(a)
+
+
+a = loadtxt("../circulate/lena.png")
+
+a.shape
